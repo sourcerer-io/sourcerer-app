1c756bc7c55cefb4036efe6d89347e6d8c86b6a1
Anatoly Kislov <anatoly@sourcerer.io>
2017-11-27 17:20:57 +0300

    chore: remove unused import

--- src/test/kotlin/test/tests/hashers/CommitHasherTest.kt
+++ src/test/kotlin/test/tests/hashers/CommitHasherTest.kt
@@ -13,7 +13,6 @@ import org.eclipse.jgit.api.Git
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.xgiven
 import java.io.File
 import java.util.stream.StreamSupport.stream
 import kotlin.streams.toList
