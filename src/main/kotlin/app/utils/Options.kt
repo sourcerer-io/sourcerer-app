cbbdf6b91ea7fe046a2c0205c601a3cc0ae70bc3
Anatoly Kislov <anatoly@sourcerer.io>
2017-12-04 15:01:28 +0300

    chore: add uninstall command to help

--- src/main/kotlin/app/utils/Options.kt
+++ src/main/kotlin/app/utils/Options.kt
@@ -8,25 +8,31 @@ import com.beust.jcommander.Parameter
 class Options {
     // Sourcerer account username.
     @Parameter(names = arrayOf("-u", "--username"),
-            description = "Sourcerer account username",
-            order = 0)
+               description = "Sourcerer account username",
+               order = 0)
     var username: String = ""
 
     // Sourcerer account password.
     @Parameter(names = arrayOf("-p", "--password"),
-            description = "Sourcerer account password",
-            order = 1)
+               description = "Sourcerer account password",
+               order = 1)
     var password: String = ""
 
     // List options and commands.
     @Parameter(names = arrayOf("-h", "--help"),
-            description = "List options and commands",
-            order = 2)
+               description = "List options and commands",
+               order = 2)
     var help: Boolean = false
 
     // Cleanup configs and run setup again.
     @Parameter(names = arrayOf("--setup"),
-            description = "Cleanup configs and run setup again",
-            order = 3)
+               description = "Cleanup configs and run setup again",
+               order = 3)
     var setup: Boolean = false
+
+    // Uninstall. Runs by install script, added to display info in help.
+    @Parameter(names = arrayOf("--uninstall"),
+               description = "Remove Sourcerer App",
+               order = 4)
+    var uninstall: Boolean = false
 }
