2b5622758e6a11e99bd364940c1fa8721809ae4b
Anatoly Kislov <anatoly@sourcerer.io>
2018-03-14 01:12:27 +0300

    chore: fix style

--- src/main/kotlin/app/utils/Misc.kt
+++ src/main/kotlin/app/utils/Misc.kt
@@ -10,7 +10,7 @@ fun <T> Sequence<T>.batch(n: Int): Sequence<List<T>> {
 }
 
 class BatchingSequence<T>(val source: Sequence<T>,
-                                  val batchSize: Int) : Sequence<List<T>> {
+                          val batchSize: Int) : Sequence<List<T>> {
     override fun iterator(): Iterator<List<T>> = object :
             AbstractIterator<List<T>>() {
         val iterate = if (batchSize > 0) source.iterator() else
