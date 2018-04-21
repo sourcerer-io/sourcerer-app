8eb5f2300e79776c6bcf32cbe9964128110f09ad
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2018-04-21 10:40:15 +0100

    feat: add fsharp support (#223) (#267)
    * Added fsharp support, #223
    
    * wip: fsharp libraries classifier to be the same as csharp

---
+++ src/main/kotlin/app/extractors/FSharpExtractor.kt
@@ -0,0 +1,61 @@
+// Copyright 2018 Sourcerer Inc. All Rights Reserved.
+// Author: Tuomas Hietanen
+// Author: Liubov Yaronskaya (lyaronskaya@sourcerer.io)
+
+package app.extractors
+
+import app.model.CommitStats
+import app.model.DiffFile
+
+class FSharpExtractor : ExtractorInterface {
+    companion object {
+        val LANGUAGE_NAME = "fsharp"
+        val FILE_EXTS = listOf("fs", "fsx")
+        // The behaviour of csharp library classifier is the same as for csharp.
+        val LIBRARIES = ExtractorInterface.getLibraries("cs")
+        val evaluator by lazy {
+            ExtractorInterface.getLibraryClassifier("csharp")
+        }
+        val importRegex = Regex("""^.*open\s+(\w+[.\w+]*)""")
+        val commentRegex = Regex("""^([^\n]*//)[^\n]*""")
+        val extractImportRegex = Regex("""open\s+(\w+[.\w+]*)""")
+    }
+
+    override fun extract(files: List<DiffFile>): List<CommitStats> {
+        files.map { file -> file.language = LANGUAGE_NAME }
+        return super.extract(files)
+    }
+
+    override fun extractImports(fileContent: List<String>): List<String> {
+        val imports = mutableSetOf<String>()
+
+        fileContent.forEach {
+            val res = extractImportRegex.find(it)
+            if (res != null) {
+                val importedName = res.groupValues[1]
+                LIBRARIES.forEach { library ->
+                    if (importedName.startsWith(library)) {
+                        imports.add(library)
+                    }
+                }
+            }
+        }
+
+        return imports.toList()
+    }
+
+    override fun tokenize(line: String): List<String> {
+        val importRegex = Regex("""^.*open\s+(\w+[.\w+]*)""")
+        val commentRegex = Regex("""^([^\n]*//)[^\n]*""")
+        var newLine = importRegex.replace(line, "")
+        newLine = commentRegex.replace(newLine, "")
+        return super.tokenize(newLine)
+    }
+
+    override fun getLineLibraries(line: String,
+                                  fileLibraries: List<String>): List<String> {
+
+        return super.getLineLibraries(line, fileLibraries, evaluator,
+            LANGUAGE_NAME)
+    }
+}
